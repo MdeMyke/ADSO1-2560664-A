dd2b18aa8695bc39fc06d23251beb98c6a4711e5
MdeMyke <116237067+MdeMyke@users.noreply.github.com>
2023-02-20 07:35:47 -0500

    Add files via upload

---
+++ Cadenasmpy/2impcadena.py
@@ -0,0 +1,6 @@
+# Solicite cadena e imprimala en todas las formas posibles en cuanto a mayusculas y minusculas
+nombre = "EL MIKE "
+nombre_lower =nombre.lower()
+print(nombre_lower)
+nombre_lower =nombre.upper()
+print(nombre_lower)
