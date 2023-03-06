6d1e23f9343b4bef4b5272c237f4639e2fdc8687
MdeMyke <116237067+MdeMyke@users.noreply.github.com>
2023-03-06 11:40:37 -0500

    Add files via upload

---
+++ Evaluaciones/Clases.py
@@ -0,0 +1,71 @@
+class pedido:
+    def __init__(self,idusuario,titulomat,codigomat):
+        self.__idusuario=idusuario
+        self.titulomat=titulomat
+        self.__codigomat=codigomat
+        self.__fechaini=[]
+        self.__fechafin=[]
+    def agregarreserva(self,fechaini,fechafin):
+        self.__fechaini.append(fechaini)
+        self.__fechafin.append(fechafin)
+    def verreservaini(self):
+        return self.__fechaini
+    def verreservafin(self):
+        return self.__fechafin
+        
+        
+    def getidusuario(self):
+        return self.__idusuario
+
+    def setidusuario(self,idusuario):
+        self.__idusuario=idusuario
+
+    def gettitulomat(self):
+        return self.titulomat
+
+    def settitulomat(self,titulomat):
+        self.titulomat=titulomat
+    
+    def getcodigomat(self):
+        return self.__codigomat
+
+    def setcodigomat(self,codigomat):
+        self.__codigomat=codigomat
+    
+    
+    
+class reserva:
+    
+    def __init__(self,fechaini,fechafin):
+        self.fechaini=fechaini
+        self.fechafin=fechafin
+
+    def getfechaini(self):
+        return self.fechaini
+    def getfechafin(self):
+        return self.fechafin
+    
+ob1=pedido(53078549,"Pepitoysucuentico",984564)
+c1=reserva('6/3/2023',())
+c2=reserva('8/4/2023',())
+ob1.agregarreserva(c1,c2)
+
+print(ob1.getidusuario())
+print(ob1.gettitulomat())
+print(ob1.getcodigomat())
+
+for Reserva in ob1.verreservaini():
+    print(Reserva.getfechaini())
+    
+for Reservaa in ob1.verreservafin():
+    print(Reservaa.getfechafin())
+
+
+del ob1
+
+print(c1.getfechaini())
+
+
+
+
+        
